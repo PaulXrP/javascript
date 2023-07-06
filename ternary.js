00e57abc2bbd01cbc85a68ad02c3e276d4248e6d
PaulXrP <121744110+PaulXrP@users.noreply.github.com>
2023-07-07 02:05:16 +0530

    Add files via upload

---
+++ ternary.js
@@ -0,0 +1,13 @@
+var authenticated = null;
+
+authenticated ? console.log("Sigout button") : console.log("Signup button");
+
+function getFee(isMember) {
+    return(isMember ? '$2.00' : '10.00');
+}
+
+console.log(getFee(true));
+
+console.log(getFee(false));
+
+console.log(getFee(null));
