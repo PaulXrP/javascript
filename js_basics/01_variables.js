3d8b067b2269e14c19ab4f6f8001b6f7083624e0
PaulXrP <121744110+PaulXrP@users.noreply.github.com>
2023-07-07 03:15:40 +0000

    done

---
+++ js_basics/01_variables.js
@@ -0,0 +1,7 @@
+let accountID = 1234
+let accountEmail = "paul@google.com"
+let accountPwd = "4567@ghjk"
+let accountState = "Gujrat"
+let accountCity
+
+console.table([accountID, accountEmail, accountPwd, accountState, accountCity])
